b5efca51d5bc80d3526009a6160687c09a96fe38
martin <adrianm.hernandez@softtek.com>
2026-05-24 20:06:11 -0600

    only 2 components

--- projects/ecommerce-components/src/public-api.ts
+++ projects/ecommerce-components/src/public-api.ts
@@ -3,13 +3,8 @@
  */
 
 // Components
-export * from './lib/components/search-bar/search-bar.component';
-export * from './lib/components/restaurant-card/restaurant-card.component';
 export * from './lib/components/category-pill/category-pill.component';
-export * from './lib/components/rating-stars/rating-stars.component';
-export * from './lib/components/cart-badge/cart-badge.component';
-export * from './lib/components/inputs/input/input.component';
-export * from './lib/components/buttons/button/button.component';
+export * from './lib/components/restaurant-card/restaurant-card.component';
 
 // Design Tokens
 export * from './lib/design-tokens/tokens';
